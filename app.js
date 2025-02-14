efc6255c9f9bdb1739f9367bc164fe5374a88266
vishnuvardhan2114 <vishnuvardhan14102002@gmail.com>
2025-02-15 00:35:58 +0530

    Inital commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Api");
+});
+
+app.listen(PORT, async () => {
+  console.log(`Subscription Api is running on port http://localhost:${PORT}`);
+  await connectToDatabase();
+});
+
+export default app;
